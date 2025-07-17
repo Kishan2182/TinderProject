e5c9817d6ef7ca549c3858b34359c7d000b2a577
Kishan2182 <kishanmishra2182002@gmail.com>
2025-07-17 23:07:24 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -3,30 +3,14 @@ const app = express();
 const PORT = 3000;
 
 //This will only handle GET call to /user
-app.get('/user', (req,res)=>{
+app.get("/user/:UserId", (req,res)=>{
+    console.log(req.params)
+    console.log(req.query)// passed using url after ? 
     res.send({firstName:"TestFirstname",lastName:"TestLastName"});
 });     
 
-app.post("/user", (req,res) => {
-    res.send("Data saved successfully to the user");
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("Data deleted successfully ");
-});
-
-app.put("/user",(req,res)=>{
-    res.send("Data replace successfully on server");
-});
-
-app.patch('/user',(req, res)=>{
-    res.send("username modified successfully");     
-});
-
-//use will match all the http  to /test
-app.use('/test',(req,res)=>{
-    res.send("I am fun controller");
-});
+//app.get(regularexpressione can be use here /.*fly$/, (req,res)=>console.log(req.params)res.send({firstName:"TestFirstname",lastName:"TestLastName"});}); 
+//app.get(we can add different  /.*fly$/, (req,res)=>console.log(req.params)res.send({firstName:"TestFirstname",lastName:"TestLastName"});}); 
 
 app.listen(PORT,()=>{
     console.log(`App is running on ${PORT}`)
