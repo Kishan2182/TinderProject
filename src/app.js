eae35a74e9b0a8819430534f13cd23fe50a6f0e7
Kishan2182 <kishanmishra2182002@gmail.com>
2025-07-26 13:26:17 +0530

    Playing with Routes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-//This will only handle GET call to /user
-app.get("/user/:UserId", (req,res)=>{
-    console.log(req.params)
-    console.log(req.query)// passed using url after ? 
-    res.send({firstName:"TestFirstname",lastName:"TestLastName"});
-});     
+const middlewarefun = (req, res, next) => {
+  const token = 999;
+  const auth = token === 999;
+  if (!auth) {
+    res.status(401).send("Unauthorized attempts to access");
+  } else {
+    console.log("Write some custom logic in between");
+    next();
+  }
+};
 
-//app.get(regularexpressione can be use here /.*fly$/, (req,res)=>console.log(req.params)res.send({firstName:"TestFirstname",lastName:"TestLastName"});}); 
-//app.get(we can add different  /.*fly$/, (req,res)=>console.log(req.params)res.send({firstName:"TestFirstname",lastName:"TestLastName"});}); 
+app.get("/user", (req, res, next) => {
+  // res.send("I am Use method");
+  console.log("This is use method");
+  next('route');
+},(req, res)=>{
+  res.send("I am actually going to skip ")
+});
 
-app.listen(PORT,()=>{
-    console.log(`App is running on ${PORT}`)
+app.get("/user", (req, res) => {
+  console.log("I am a user route");
+  res.send("I am called ");
+});
+
+app.get("/user/1",(req ,res)=>{
+  console.log("Skip routes using next()")
+  res.send("Skip routes using next('Route')")
+})
+app.get(
+  "/example",
+  (req, res, next) => {
+    console.log("I am 1st method");
+    // res.send("I am first method");
+    next();
+  },
+  [
+    (req, res, next) => {
+      console.log("I am 2nd method");
+      next();
+    },
+    (req, res) => {
+      console.log("I am 3rd method");
+      res.send("I am  3rd method");
+    },
+  ]
+);
+
+app.get("/example2", middlewarefun, (req, res) => {
+  res.send("I am example 2 ");
+});
+
+// we can pass array of methods  in the http method
+
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
 });
