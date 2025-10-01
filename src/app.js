b0daf6b04d560a5258d3ba9f5841879d05953ebb
Kishan2182 <kishanmishra2182002@gmail.com>
2025-10-01 21:24:39 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -21,39 +21,71 @@ app.post("/signup", async (req, res) => {
     res.send("User created Successfully");
   } catch (error) {
     console.log("error" + error.message);
-    res.send("error creating user");
+    res.send("error creating user" + error.message);
   }
 });
 
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
-  console.log(userEmail)
+  console.log(userEmail);
 
   try {
-    const userdetail = await User.findOne({emailId:userEmail});
-    if(!userdetail){
+    const userdetail = await User.findOne({ emailId: userEmail });
+    if (!userdetail) {
       res.status(404).send("user not found");
-    }else{
+    } else {
       res.status(200).send(userdetail);
     }
-
   } catch (error) {
     res.status(500).send("something went wrong");
   }
 });
 
-app.get("/feed",async (req, res)=>{
-
+app.get("/feed", async (req, res) => {
   try {
     const userDetail = await User.find({});
-    if(userDetail.length===0){
-      res.status(404).send("user not found")
-    }else{
+    if (userDetail.length === 0) {
+      res.status(404).send("user not found");
+    } else {
       res.status(200).send(userDetail);
     }
-
   } catch (error) {
     res.status(500).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  console.log(userId);
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    //const user = await User.findOneAndDelete({_id:userId});
+    res.send("User deleted Successfully");
+  } catch (error) {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  console.log(userId);
+  const data = req.body;
+  console.log(data);
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    // const user = await User.findOneAndUpdate(
+    //   { emailId: email },
+    //   { fistName: fistName },
+    //   { returnDocument: "after" }
+    // );
+    console.log(user);
+    res.send("User updated Successfully");
+  } catch (error) {
+    res.status(500).send("Something went wrong" + error.message);
   }
 });
 
--- src/model/user.js
+++ src/model/user.js
@@ -1,24 +1,54 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  fistName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+      maxLength: 20,
+    },
+    emailId: {
+      type: String,
+      trim: true,
+      unique: true,
+      lowercase: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      minLength: 8,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error();
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://p.kindpng.com/picc/s/252-2524695_dummy-profile-image-jpg-hd-png-download.png",
+    },
+    about: {
+      type: String,
+      default: "I am deafult description of the about",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
